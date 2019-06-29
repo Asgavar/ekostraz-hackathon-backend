e120aec47f687c84abbdee12c2c187dc894e0d86
Konrad Bernaszuk <konradbernaszuk@gmail.com>
2019-06-29 22:22:42 +0200

    Intervention post, and scan update

--- interventions_get.py
+++ interventions_get.py
@@ -2,14 +2,16 @@ import json
 
 import boto3
 from boto3.dynamodb.conditions import Key
+from botocore.docs import paginator
 
 
 def handler(event, _):
     dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
     table = dynamodb.Table('interventions')
 
-    response = table.query(
-        KeyConditionExpression=Key('id').eq(event['id'])
-    )
+    if len(event) == 0:
+        response = table.scan()
+    else:
+        response = table.query(KeyConditionExpression=Key('id').eq(event['id']))
 
     return response['Items']
